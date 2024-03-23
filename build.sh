4f29bde74414978c7ae0c689e56a2130f502877c
Shubham Sangwan <79131438+shubhamSangwan01@users.noreply.github.com>
2024-03-24 02:38:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,7 @@
-#!/bin/bash
-
-# Create a virtual environment
-python3 -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Upgrade pip
-pip install --upgrade pip
-
-# Install project dependencies
 pip install -r requirements.txt
+echo("1")
+python manage.py migrate
 
-# Deactivate the virtual environment
-deactivate
-
-# Build static files (if applicable)
 python manage.py collectstatic --noinput
 
-# Perform migrations (if applicable)
-python manage.py migrate
+python manage.py runserver
